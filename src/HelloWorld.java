666e15f503464b9e967fa1e63c43b0b947184125
Anthony Fortney <afortney86@gmail.com>
2016-11-11 11:42:46 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by anthonyfortney on 11/11/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        // window.console.log
+        // always use double quotes ""
+
+        System.out.println("Hello world!!");
+    }
+}
