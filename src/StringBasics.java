8436d57fa1c7310a76a3fb93810cb23614355e31
Anthony Fortney <afortney86@gmail.com>
2016-11-14 13:10:51 -0600

    StringBasics Exercises

---
+++ src/StringBasics.java
@@ -0,0 +1,24 @@
+/**
+ * Created by anthonyfortney on 11/14/16.
+ */
+public class StringBasics {
+    public static void main(String[] args) {
+
+        String message_a = "We don't need no education\nWe don't need no thought control";
+
+        System.out.println(message_a);
+
+        String message_b = "Check \"this\" out!, \"s inside of \"s!";
+
+            System.out.println(message_b);
+
+          String message_c = "In windows, the main drive is usually C:\\";
+
+        System.out.println(message_c);
+
+        String message_d = "I can do back slashes \\, and double back slashes \\\\, and the amazing triple back-slash \\\\\\!";
+
+                System.out.println(message_d);
+    }
+
+}
