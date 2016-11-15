4bfd6b07351ebcd93ce54d3f1b5dbdf9f8758642
Anthony Fortney <afortney86@gmail.com>
2016-11-14 20:00:49 -0600

    Factorial Exercise

---
+++ src/FactorialNumber.java
@@ -0,0 +1,36 @@
+/**
+ * Created by anthonyfortney on 11/14/16.
+ */
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+        String userInput = "n";
+
+        Scanner input = new Scanner(System.in);
+        do {
+            //Gives prompt
+            System.out.print("Enter a number to find the factorial of it ");
+
+            //Enter the times you want to run
+            int number = input.nextInt();
+
+            //Declares new int
+            long factor = 1;
+
+            //Runs loop and multiplies factor each time run
+            for (int i = 1; i <= number; i++) {
+                factor = factor * i;
+            }
+
+            //Prints out final number
+            System.out.println(factor);
+
+            //Prompts for another number
+            System.out.println("Would you like to enter another number?");
+            userInput = input.next();
+        }while (userInput.equals("y"));
+    }
+
+}
---
+++ src/TableOfPowers.java
@@ -0,0 +1,8 @@
+/**
+ * Created by anthonyfortney on 11/14/16.
+ */
+public class TableOfPowers {
+    public static void main(String[] args) {
+
+    }
+}
