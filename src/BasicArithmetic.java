18022551c654afa3e4ade6fcda0d92677cbb89f0
Anthony Fortney <afortney86@gmail.com>
2016-11-14 19:17:36 -0600

    Basic Arithmetic Exercises

---
+++ src/BasicArithmetic.java
@@ -0,0 +1,40 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+/**
+ * Created by anthonyfortney on 11/14/16.
+ */
+public class BasicArithmetic {
+    public static void main(String[] args){
+
+        System.out.println(addition(1, 1));
+
+        System.out.println(subtraction(3, 1));
+
+        System.out.println(multiply(2, 1));
+
+        System.out.println(divide(2, 2));
+
+        System.out.println(mod(2, 1));
+
+    }
+
+    private static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    private static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    private static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    private static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    private static int mod(int num1, int num2) {
+        return num1 % num2;
+    }
+}
