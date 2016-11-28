9d5c3ae9524e70dffa512a7a8ddc9e8c15cb35e6
Anthony Fortney <afortney86@gmail.com>
2016-11-28 17:07:11 -0600

    Student Class w/sorting code

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by anthonyfortney on 11/28/16.
+ */
+
+
+
+public class Student implements Comparable<Student> {
+
+    @Override
+    public int compareTo(Student o) {
+        if (score == o.score) return 0;
+        if (score > o.score) return 1;
+        return -1;
+    }
+
+    String firstName;
+    String lastName;
+    int score;
+
+    public Student(String firstName, String lastName, int score) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.score = score;
+    }
+
+//    Collections.sort(students, );
+}
