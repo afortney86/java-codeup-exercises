75b3dc48118601c6baeefa03d81563de6c953224
Anthony Fortney <afortney86@gmail.com>
2016-11-30 14:31:25 -0600

    Methods for circle class

--- src/Circle.java
+++ src/Circle.java
@@ -5,38 +5,45 @@
  *  To find area: A=PiR^2
  */
 
-import java.util.Scanner;
-
 public class Circle {
-    double radius;
+
+    double radius = 0;
+    double circumference = 0;
+    double area = 0;
+
 
     public Circle(double radius) {
-
+        this.radius = radius;
     }
 
 
     public double getCircumference(){
+        this.circumference = 2 * (Math.PI * radius);
+        return circumference;
 
     }
 
     public String getFormattedCircumference(){
-
+        String formatted_C_Output = String.format("%.2f", circumference);
+        return formatted_C_Output;
     }
 
     public double getArea(){
-
+        this.area = Math.PI * (radius * radius);
+        return area;
     }
 
     public String getFormattedArea(){
-
+        String formatted_A_Output = String.format("%.2f", area);
+        return formatted_A_Output;
     }
 
-    private String formatNumber(double x){
-
-    }
-
-    public static int getObjectCount(){
-
-    }
+//    private String formatNumber(double x){
+//
+//    }
+//
+//    public static int getObjectCount(){
+//
+//    }
 
 }
