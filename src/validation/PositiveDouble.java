08d83762ae7f41f021f96bcdc7ccbb6a770a1f30
Anthony Fortney <afortney86@gmail.com>
2016-12-02 17:43:54 -0600

    Positive double code written

--- src/validation/PositiveDouble.java
+++ src/validation/PositiveDouble.java
@@ -3,5 +3,23 @@ package validation;
 /**
  * Created by Moravia on 12/2/16.
  */
-public class PositiveDouble {
+public class PositiveDouble implements Validator {
+
+    @Override
+    public boolean isValid(Object instance) {
+
+        if (instance instanceof Double && ((double) instance >= 0)) {
+
+            return true;
+
+        } else return false;
+
+    }
+
+    @Override
+    public String errorMessage() {
+
+        return "Number must be a positive Integer.";
+
+    }
 }
