e1a08179734a0129386b694bc98b0bb11077e526
Anthony Fortney <afortney86@gmail.com>
2016-12-05 12:11:09 -0600

    Cloning sheep counting alligators

--- src/CountingAlligatorsCloningSheep/Sheeps.java
+++ src/CountingAlligatorsCloningSheep/Sheeps.java
@@ -4,4 +4,12 @@ package CountingAlligatorsCloningSheep;
  * Created by anthonyfortney on 12/5/16.
  */
 public interface Sheeps {
+
+    void incrementCount();
+
+    void resetCount();
+
+    int getCount();
+
+    String getCountString();
 }
